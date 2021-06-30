c2ccd82b7863f3f0e1d8cf24364ed09ddc4ab039
WMXPY <wm@mengw.io>
2021-06-30 13:09:39 -0500

    init generator

--- src/generator.ts
+++ src/generator.ts
@@ -4,10 +4,28 @@
  * @description Identifier
  */
 
+import { Words } from "@sudoo/words";
+import { englishWords } from "@sudoo/words-english";
+
 export class ReadableIdentifierGenerator {
 
-    public static create(): ReadableIdentifierGenerator {
+    public static from(words: Words): ReadableIdentifierGenerator {
 
-        return new ReadableIdentifierGenerator();
+        return new ReadableIdentifierGenerator(words);
+    }
+
+    public static default(): ReadableIdentifierGenerator {
+
+        return new ReadableIdentifierGenerator(englishWords);
+    }
+
+    private readonly _words: Words;
+
+    private constructor(words: Words) {
+
+        this._words = words;
+    }
+
+    public generatePair(identifier: string): string {
     }
 }
