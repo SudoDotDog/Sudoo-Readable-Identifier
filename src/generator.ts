1b075fecfba2156a247f9b49e48d10cc87c968eb
WMXPY <wm@mengw.io>
2021-06-30 14:10:08 -0500

    imrpove gnereator optionss

--- src/generator.ts
+++ src/generator.ts
@@ -8,38 +8,88 @@ import { Words } from "@sudoo/words";
 import { SplitedPairIdentifier, SplitedTupleIdentifer } from "./declare";
 import { splitPairIdentifier, splitTupleIdentifier } from "./util";
 
+export type ReadableIdentifierGeneratorOptions = {
+
+    readonly join: string;
+    readonly capital: boolean;
+};
+
 export class ReadableIdentifierGenerator {
 
-    public static from(words: Words): ReadableIdentifierGenerator {
+    public static from(words: Words, options?: Partial<ReadableIdentifierGeneratorOptions>): ReadableIdentifierGenerator {
 
-        return new ReadableIdentifierGenerator(words);
+        const fixedOptions: ReadableIdentifierGeneratorOptions = {
+
+            join: '-',
+            capital: false,
+            ...options,
+        };
+
+        return new ReadableIdentifierGenerator(words, fixedOptions);
     }
 
     private readonly _words: Words;
+    private readonly _options: ReadableIdentifierGeneratorOptions;
 
-    private constructor(words: Words) {
+    private constructor(words: Words, options: ReadableIdentifierGeneratorOptions) {
 
         this._words = words;
+        this._options = options;
     }
 
-    public generatePair(identifier: string, join: string = "-"): string {
+    public generatePair(identifier: string): string {
 
         const pair: SplitedPairIdentifier = splitPairIdentifier(identifier);
 
-        const adjective: string = this._words.getAdjectiveSet().getStringRemainderBasedWord(pair.left);
-        const noun: string = this._words.getNounSet().getStringRemainderBasedWord(pair.right);
+        const adjective: string = this._getAdjective(pair.left);
+        const noun: string = this._getNoun(pair.right);
 
-        return `${adjective}${join}${noun}`;
+        return [adjective, noun].join(this._options.join);
     }
 
-    public generateTuple(identifier: string, join: string = "-"): string {
+    public generateTuple(identifier: string): string {
 
         const tuple: SplitedTupleIdentifer = splitTupleIdentifier(identifier);
 
-        const adjective: string = this._words.getAdjectiveSet().getStringRemainderBasedWord(tuple.left);
-        const verb: string = this._words.getVerbSet().getStringRemainderBasedWord(tuple.middle);
-        const noun: string = this._words.getNounSet().getStringRemainderBasedWord(tuple.right);
+        const adjective: string = this._getAdjective(tuple.left);
+        const verb: string = this._getVerb(tuple.middle);
+        const noun: string = this._getNoun(tuple.right);
 
-        return `${adjective}${join}${verb}${join}${noun}`;
+        return [adjective, verb, noun].join(this._options.join);
+    }
+
+    private _getAdjective(component: string): string {
+
+        const adjective: string = this._words.getAdjectiveSet().getStringRemainderBasedWord(component);
+
+        if (this._options.capital) {
+            return this._capitalize(adjective);
+        }
+        return adjective;
+    }
+
+    private _getVerb(component: string): string {
+
+        const verb: string = this._words.getVerbSet().getStringRemainderBasedWord(component);
+
+        if (this._options.capital) {
+            return this._capitalize(verb);
+        }
+        return verb;
+    }
+
+    private _getNoun(component: string): string {
+
+        const noun: string = this._words.getNounSet().getStringRemainderBasedWord(component);
+
+        if (this._options.capital) {
+            return this._capitalize(noun);
+        }
+        return noun;
+    }
+
+    private _capitalize(value: string): string {
+
+        return `${value.substring(0, 1).toUpperCase()}${value.substring(1, value.length).toLowerCase()}`;
     }
 }
